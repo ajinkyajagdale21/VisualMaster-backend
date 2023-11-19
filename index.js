5d2cb16d74c275a398bbaa9649f54c273e8ce800
Ajinkyaj21 <ajinkya.jagadale@flairminds.com>
2023-11-19 21:17:32 +0530

    added vercel.json change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000; 
 
 
-const allowedOrigins = ['https://visual-master-frontend.vercel.app'];
+const allowedOrigins = ['https://visual-master-frontend.vercel.app/'];
 
 const corsOptions = {
   origin: function (origin, callback) {
